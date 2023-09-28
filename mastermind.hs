dbfb9c57a1f0455f436ff20640790332e0e77a9d
Lazlo-coder <lasarodalmeida@gmail.com>
2023-09-27 21:48:59 -0300

    finalizado

--- mastermind.hs
+++ mastermind.hs
@@ -1,8 +1,10 @@
 -- Dener Luis Basilio Theodoro 201835001
 -- Lásaro de Almeida Deodoro   201835004
+
 import System.Random
 
 digitos = ['1','2','3','4','5','6','7','8','9','0']
+separador = "============================================================================"
 
 randCarater = do
     gen <- newStdGen
@@ -28,7 +30,7 @@ inverteSaida tentativa (x:xs) = tentativa !! x : (inverteSaida tentativa xs)
 trocaIndice e i lista
   | i >= (length lista) = lista
   | otherwise = auxTrocaIndice e i lista 0
-
+  
 auxTrocaIndice  e i (x:xs) j
   | length (x:xs) <= 0 = []
   | otherwise =
@@ -58,15 +60,14 @@ auxGeraSegredo segredo repetidos
 comparaSegredoEntrada entrada segredo = auxCompara entrada segredo entrada 0
 auxCompara [] segredo resposta _ = comparaSegredoFim segredo resposta
 auxCompara (x:xs) segredo resposta i =
-
     if (segredo !! i) ==  (resposta !! i)
         then 
             auxCompara xs (trocaIndice 'O' i segredo) (trocaIndice 'O' i resposta) (i+1)
     else
         auxCompara xs segredo resposta (i+1)
 
-comparaSegredoFim entrada segredo = auxComparaSegredoFim entrada segredo entrada 0
 
+comparaSegredoFim entrada segredo = auxComparaSegredoFim entrada segredo entrada 0
 auxComparaSegredoFim [] _ resposta _ = resposta
 auxComparaSegredoFim (x:xs) segredo resposta i =
     if x == 'O'
@@ -82,7 +83,7 @@ auxComparaSegredoFim (x:xs) segredo resposta i =
 
 
 perguntaSegredo tentativas segredo
-    | tentativas <= 0 = putStrLn $ "Você perdeu! O segredo era: " ++ segredo
+    | tentativas <= 0 = putStrLn $ separador ++"\nVocê perdeu! O segredo era: " ++ segredo
     | otherwise = do
         tentativaSegredo <- adivinhaSegredo tentativas segredo
         if segredo /= tentativaSegredo
@@ -90,14 +91,15 @@ perguntaSegredo tentativas segredo
                 posicoes <- randPositions
                 let trocaCorretos = (comparaSegredoEntrada tentativaSegredo segredo)
                 let saida = inverteSaida trocaCorretos posicoes
-                putStrLn $ "Resposta: " ++ saida ++ (show posicoes)
+                putStrLn $ "\nResposta: " ++ saida
                 perguntaSegredo (tentativas - 1) segredo
-            else putStrLn "Jogador 2 adivinhou o segredo corretamente!"
+        else putStrLn $ separador ++"\nJogador 2 adivinhou o segredo corretamente!"
 
 
 adivinhaSegredo valor segredo = do
+    putStrLn $ separador
     putStrLn "Chute um segredo (digitos de 0 a 9, totalizando 4 digitos)"
-    let msg = "Você tem " ++ show valor ++ " tentativas restantes"
+    let msg = "Você tem " ++ show valor ++ " tentativas restantes! \nTentativa: "
     tentativaSegredo <- validaTentativa msg
     return tentativaSegredo
 
@@ -119,7 +121,7 @@ validaCaracteresEntrada [] = True
 validaCaracteresEntrada (x:xs) =  if (find x digitos) /= (-1)  then validaCaracteresEntrada xs else False 
 
 
-validaEntrada str = do
+ehRepetido str = do
     putStrLn $ str
     entrada <- getLine
     if entrada == "s" || entrada == "S"
@@ -128,15 +130,15 @@ validaEntrada str = do
         if entrada == "n" || entrada == "N"
             then return 0
         else
-        validaEntrada "Digite uma opção válida!"
+        ehRepetido "Digite uma opção válida!"
 
 main:: IO ()
 main = do
-    putStrLn "====================================================================================="
-    putStrLn "|--------------------------------- MASTERMIND --------------------------------------|"
-    putStrLn "====================================================================================="
+    putStrLn $ separador
+    putStrLn "|--------------------------------- MASTERMIND -----------------------------|"
+    putStrLn $ separador
     let msgInicio = "Jogar com números repetidos? \n (S ou N)"
-    repetidos <- validaEntrada msgInicio
+    repetidos <- ehRepetido msgInicio
     segredo <- geraSegredo repetidos
     putStrLn "É hora do Jogador 2 tentar adivinhar"
-    perguntaSegredo 8 segredo
+    perguntaSegredo 8 "segredo"
